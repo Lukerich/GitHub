c166b5668b1632b3f687f43d27787c55a0866391
lukerich <r.l.luke@fullsail.edu>
2013-04-21 17:18:52 -0400

    update

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -10,3 +10,9 @@
     // Price of fuel per gallon
     // Miles per gallon of vehicle
 
+    var miles = prompt ("Enter the Distance in Mile", "1");
+    var ppg = prompt ("Enter the Price of fuel/gallon", "1");
+    var mpg = prompt("Enter the vehicle's MPG", "1");
+
+    var trpExp =
+
