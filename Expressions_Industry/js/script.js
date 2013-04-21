5c46ef8f6d74b9bdafbfaf1ea48e885e39f134ef
lukerich <r.l.luke@fullsail.edu>
2013-04-21 18:13:49 -0400

    updates

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -11,3 +11,4 @@
 
 var webAlen = prompt ("Enter length in pixels of page area", "1"); // Prompt for web page area length in pixels
 var webAdep = prompt("Enter depth in pixels of page area", "1"); // Prompt for web page area depth in pixels
+var charSze = prompt("Enter size of characters in pixels", "1"); // Prompt for character size in pixels
