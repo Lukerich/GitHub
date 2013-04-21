c9ebed9c5bd4ae8f4f1e3a4f9cf55d9ba737a79c
lukerich <r.l.luke@fullsail.edu>
2013-04-21 16:52:33 -0400

    refresh

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,4 +3,9 @@
  * Date: 4/21/13
  * Expressions Expressions_Industry
  */
-   //
+   //     Calculating the number of characters that can fit on a area of a web page.
+    //      Three variable inputs are needed
+        // Area of web page in number of pixels length
+        // Area of web page in number of pixels depth
+        // Size of characters in pixels
+
--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,4 +3,10 @@
  * Date: 4/21/13
  * Expressions Expressions_Personal
  */
- //
+ //  The cost of driving from one to another
+
+// Three variables are
+    // Distance in miles
+    // Price of fuel per gallon
+    // Miles per gallon of vehicle
+
