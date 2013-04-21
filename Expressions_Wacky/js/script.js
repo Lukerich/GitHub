6eff255b2810c55eb22024e2427c5dd1120c9a5f
lukerich <r.l.luke@fullsail.edu>
2013-04-21 19:39:08 -0400

    updates

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,3 +3,11 @@
  * Date: 4/21/13
  * Expressions - Expressions_Wacky
  */
+           // To calculate the number of shovel full of dirt to bury a coffin
+/** Input prompts
+    coffin size in area (cubic feet)
+    burial site in area (cubic feet)
+    shovel full of dirt in pounds (converted to area cubic feet)
+ */
+
+var coffin = prompt ()
