b05b4128fb5ae356231350c0e36be195261d4037
lukerich <r.l.luke@fullsail.edu>
2013-04-21 20:10:24 -0400

    updates

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,7 +3,8 @@
  * Date: 4/21/13
  * Expressions - Expressions_Wacky
  */
-           // To calculate the number of shovel full of dirt to bury a coffin
+           // To calculate the number of shovels full of dirt to bury a coffin
+
 /** Input prompts
  *  Greeting with text string
  *  coffin size in area (cubic feet)
@@ -14,3 +15,5 @@
     var grtStr = prompt("What is your name?", "Igor?");
     var coffin = prompt ("Enter the dimensions of the coffin \n in cubic feet \n ex. a 6' tall medium build \n person would need \n a coffin of 66.5 cubic feet", "66.5 ft\xB3");
     var burSte = prompt ("Enter the dimensions of the burial site in cubic feet \n ex. a coffin of 66.5 ft \xB3 \n would need a site of 266 ft\xB3");
+    var shvWgt = prompt ("Enter the weight in pounds each shovel full will be", "3 lb");
+
