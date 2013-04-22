cf05e09695ed3dddc75770e5e2d2214f3ba58e4b
lukerich <r.l.luke@fullsail.edu>
2013-04-21 21:03:04 -0400

    updates

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -13,12 +13,12 @@
  */
 
     var grtStr = prompt("What is your name?", "Igor?");
-    var coffin = prompt ("Enter the dimensions of the coffin \n in cubic feet \n ex. a 6' tall medium build \n person would need \n a coffin of 66.5 cubic feet", "66.5 ft\xB3");
-    var burSte = prompt ("Enter the dimensions of the burial site in cubic feet \n ex. a coffin of 66.5 ft \xB3 \n would need a site of 266 ft\xB3", "266 ft\xB3");
-    var shvWgt = prompt ("Enter the weight in pounds each shovel full will be", "8lb");
+    var coffin = prompt ("Enter the dimensions of the coffin \n in cubic feet \n ex. a 6' tall medium build \n person would need \n a coffin of 66.5 cubic feet", "66.5");
+    var burSte = prompt ("Enter the dimensions of the burial site in cubic feet \n ex. a coffin of 66.5 ft \xB3 \n would need a site of 266 ft\xB3", "266");
+    var shvWgt = prompt ("Enter the weight in pounds each shovel full will be", "8");
 
             var shvAre = shvWgt / 100;
-            var numshvFl = (shvAre / burSte) * 2 - (shvAre / coffin);
+            var numshvFl = (shvAre / burSte) * 2 - shvAre / coffin;
 
     console.log("Luke's mortuary - you stab em we slab em - some go to heaven - some go to hello " + [grtStr] + " \n number of shovels full of dirt needed \n to bury the coffin is " + [numshvFl]);
     alert("Luke's mortuary - you stab em we slab em - some go to heaven - some go to hello " + [grtStr] + " \n the number of shovels full of dirt needed \n to bury the coffin is " + [numshvFl]);
