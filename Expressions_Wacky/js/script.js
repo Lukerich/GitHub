b71059f919282e12321e88e58136105304cde73a
lukerich <r.l.luke@fullsail.edu>
2013-04-21 20:59:03 -0400

    updates

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -14,7 +14,7 @@
 
     var grtStr = prompt("What is your name?", "Igor?");
     var coffin = prompt ("Enter the dimensions of the coffin \n in cubic feet \n ex. a 6' tall medium build \n person would need \n a coffin of 66.5 cubic feet", "66.5 ft\xB3");
-    var burSte = prompt ("Enter the dimensions of the burial site in cubic feet \n ex. a coffin of 66.5 ft \xB3 \n would need a site of 266 ft\xB3");
+    var burSte = prompt ("Enter the dimensions of the burial site in cubic feet \n ex. a coffin of 66.5 ft \xB3 \n would need a site of 266 ft\xB3", "266 ft\xB3");
     var shvWgt = prompt ("Enter the weight in pounds each shovel full will be", "8lb");
 
             var shvAre = shvWgt / 100;
