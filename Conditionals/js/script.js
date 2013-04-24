ba2b27abf9e9927efb200437fc2bce76c2a35b8a
lukerich <r.l.luke@fullsail.edu>
2013-04-23 21:55:03 -0400

    updates

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -36,11 +36,12 @@
     // Relational Expressions (Comparing object for decision making)
 
     var kidHeight = 50;
+    var minHeight = 48;
 
     // if the child is old enough print to the console "you can ride!"
     // if the kid is over 48 inches in height
 
-        if(kidHeight > 48){
+        if(kidHeight > minHeight){
             console.log ("You can ride the coaster");    // code performed if the condition is true (code block)
         }
 
