b59df6230bc5a7cb6ea0fc2a46ad7e800d12c5e2
lukerich <r.l.luke@fullsail.edu>
2013-04-24 23:30:59 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/chkLogin.js
+++ Conditionals_worksheet/js/chkLogin.js
@@ -3,5 +3,17 @@
  * Date: 4/22/13
  * Check the Login
  * Conditionals_worksheet
+ * validate correct user and password. If username doesn't match use one message
+ * if password doesn't match use a different message
+ * Only one error message should appear
+ * Given:
+ *      Username entered by user
+ *      Password entered by user
+ *      Correct username
+ *      Correct password
+ * Results to print out:
+ *      "Welcome, (place their username here)!" - if the username and password are correct
+ *      "User not found. Try again." - if username does not match
+ *      "Password does not match our records." - if password does not match but username does
  */
 
