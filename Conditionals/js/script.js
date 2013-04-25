9cc820091d88bbd0a8507c5f7e53e660af1e0e62
lukerich <r.l.luke@fullsail.edu>
2013-04-24 22:12:30 -0400

    updates

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -105,10 +105,16 @@
     var iPhonePrice = 199.99;
     var payCheck = 200;
 
+    // if the price of the phone is less then our budget AND if our paycheck is over 300
+
     if(iPhonePrice < budget){
         console.log("We can buy the phone!");
     }else{
         console.log("No phone for you!!");
     }
-
+     if(payCheck > 300){
+         console.log("We can buy the phone!");
+     }else{
+         console.log("No phone for you!!");
+     }
 
