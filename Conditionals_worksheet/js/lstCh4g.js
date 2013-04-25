1b418511d0de33bf3602f08ec6301a245ab2cb6e
lukerich <r.l.luke@fullsail.edu>
2013-04-24 23:40:44 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/lstCh4g.js
+++ Conditionals_worksheet/js/lstCh4g.js
@@ -3,5 +3,18 @@
  * Date: 4/22/13
  * Last Chance for Gas!
  * Conditionals_worksheet
+ *
+ *  Driver has to determine if they can make it across the desert with current fuel
+ *  They are about to pass the last gas station for the next 200 miles
+ *  they need to determine whether they should stop for gas
+ *
+ *  Given:
+ *       Gas efficiency of car (in mpg)
+ *       Gauge reading of gas tank (in%)
+ *       Car's gas tank capacity (in gallons)
+ *  Results to print out:
+ *       "Yes, you can make it without stopping for gas!" or "You only have X gallons of gas in your tank, better get
+ *       gas now while you can!"
+ *
  */
 
