dfc4948b02bc1a1643dd489132b885528a34afaf
lukerich <r.l.luke@fullsail.edu>
2013-04-24 23:35:36 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/grdLetCal.js
+++ Conditionals_worksheet/js/grdLetCal.js
@@ -3,5 +3,16 @@
  * Date: 4/22/13
  * Grade Letter Calculator
  * Conditionals_worksheet
+ * Student earns a number grade at the conclusion of a course at Full Sail
+ * Determine the appropriate letter grade for the number using conditional statements
+ *
+ * Assume grades are whole numbers that never go below 0 or above 100
+ * Use the FullSail Grade Scale
+ * There should be only one print out to the console
+ *
+ * Given:
+ *      Grade (in percent)
+ * Result to print out
+ *      "You have a X%, which means you have earned a(n) X in the class!"
  */
 
