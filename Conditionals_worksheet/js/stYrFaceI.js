53e91c88219d56043c3d69a1047fc314d80a3bde
lukerich <r.l.luke@fullsail.edu>
2013-04-25 18:09:29 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/stYrFaceI.js
+++ Conditionals_worksheet/js/stYrFaceI.js
@@ -3,5 +3,11 @@
  * Date: 4/22/13
  * Stuff your face I
  * Conditionals_worksheet
+ * Entrant must weigh 250lbs or more
+ * Given:
+ *      Competitor's weight
+ * Result:
+ *      "The competitor qualifies for the heavyweight division." Or "The competitor needs to gain some weight!"
+ *
  */
 
