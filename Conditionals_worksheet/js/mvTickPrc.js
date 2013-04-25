bfd501d5ee74fae39ab294ba0ea973d91d864fa4
lukerich <r.l.luke@fullsail.edu>
2013-04-24 23:25:54 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/mvTickPrc.js
+++ Conditionals_worksheet/js/mvTickPrc.js
@@ -3,5 +3,14 @@
  * Date: 4/22/13
  * Movie Ticket Price
  * Conditionals_worksheet
+ * Local theater has ticket price of $12.00 but if your are senior (55 and older), or under 10 you get the
+ * discounted price of $7.00. Additionally, if seeing a movie from 3pm to 5pm you get the discounted price.
+ * which of the two prices are customers eligible for
+ * Given:
+ *      Time of the movie (assume whole numbers)
+ * Age of customer:
+ * Results to print out:
+ *      "The ticket price is X"
+ *
  */
 
--- Conditionals_worksheet/js/tirePrsI.js
+++ Conditionals_worksheet/js/tirePrsI.js
@@ -3,5 +3,12 @@
  * Date: 4/22/13
  * Tire Pressure I
  * Conditionals_worksheet
+ *
+ * Front tires need to be the same pressure - back tires need to be the same pressure - front to back can be different
+ * Use a single conditional that would determine if the tires of a given car are up to spec.
+ * Given:
+ *      Pressure for each tire of the car (in psi) in an array
+ * Result to Print Out:
+ *      "The tires pass spec!" or "Get your tires checked out!"
  */
 
