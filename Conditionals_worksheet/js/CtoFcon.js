ee6eae5cef190fc9d8341695a464537c878632db
lukerich <r.l.luke@fullsail.edu>
2013-04-25 18:21:24 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/CtoFcon.js
+++ Conditionals_worksheet/js/CtoFcon.js
@@ -3,7 +3,18 @@
  * Date: 4/22/13
  * Celsius to Fahrenheit converter
  * Conditionals_worksheet
- *
- *
+ *  Convert a temperature to either Celsius or degrees Fahrenheit depending on what the user has entered.
+ *  If the user puts a "C" for the unit, the calculator should convert Celsius. It the user puts "F" for the unit,
+ *  the calculator should convert to Fahrenheit.
+ *  Given:
+ *       Degrees (in F or C)
+ *  Unit:
+ *      (a string holding "F" or a "C")
+ *  Result:
+ *        "The temperature is X degrees Celsius." Or "The temperature is X degrees Fahrenheit."
+ *  Data Set to Test:
+ *         32F is 0C
+ *         100C is 212F
+ *         90F is 32.22C
  */
 
