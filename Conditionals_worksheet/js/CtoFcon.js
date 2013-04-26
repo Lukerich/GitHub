ae8f1cfcf351bfd5873622f4f6a4e163c7593c8c
lukerich <r.l.luke@fullsail.edu>
2013-04-25 23:43:39 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/CtoFcon.js
+++ Conditionals_worksheet/js/CtoFcon.js
@@ -22,13 +22,13 @@
     degUnt = prompt("Enter the Unit to convert to: \nC for Celsius F for Fahrenheit", "");
               // double if statements that use strict equality and or operators to
               // evaluate user input and decide what formula to run
-    if (degUnt === "C" || degUnt === "c"){
-            tmptre = (degTmp - 32) * 5/9;        // formula to convert
-            console.log("The temperature is " + tmptre + "\xB0 degrees Celsius.");
+    if (degUnt === "C" || degUnt === "c"){       // or operator used to account for upper and lower case
+            tmptre = (degTmp - 32) * 5/9;        // formula to convert fahrenheit to celsius
+            console.log("The temperature is " + tmptre + "\xB0 degrees Celsius.");  //print to console
     }
-    if (degUnt === "F" || degUnt === "f"){
-             tmptre = degTmp * 9/5 + 32;
-             console.log("The temperature is " + tmptre + "\xB0 degrees Fahrenheit.");
+    if (degUnt === "F" || degUnt === "f"){         // or operator used to account for upper and lower case
+             tmptre = degTmp * 9/5 + 32;          // formula to convert celsius to fahrenheit
+             console.log("The temperature is " + tmptre + "\xB0 degrees Fahrenheit."); // print to console
     }
 
 
