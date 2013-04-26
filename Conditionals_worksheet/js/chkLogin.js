0967bfd447fe120df1fea163e64dad9aff442e76
lukerich <r.l.luke@fullsail.edu>
2013-04-25 22:32:13 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/chkLogin.js
+++ Conditionals_worksheet/js/chkLogin.js
@@ -26,13 +26,10 @@
         usrNam[0] = new Array();
         usrNam[0][0] = "bill";
         usrNam[0][1] = "123";
-        usrNam[0][2] = "bob";
-        usrNam[0][3] = "2we";
-        usrNam[0][4] = "sam";
-        usrNam[0][5] = "456";
 
-            if (psswd === usrNam[0][1,2,5]){
-                (usrnm === usrNam[0][0,2,4])? console.log("Welcome, " + usrnm + "!") : console.log("User not found. Try again");
+
+            if (psswd === usrNam[0][1]){
+                (usrnm === usrNam[0][0])? console.log("Welcome, " + usrnm + "!") : console.log("User not found. Try again");
             }else{
                 console.log("Password does not match our records. ");
             }
