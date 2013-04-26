11f0cbcb2cb64f1ba37968c2fc65d11281ba3b64
lukerich <r.l.luke@fullsail.edu>
2013-04-25 22:59:08 -0400

    Class assignment Conditional_worksheet

--- Conditionals_worksheet/js/tirePrsI.js
+++ Conditionals_worksheet/js/tirePrsI.js
@@ -11,4 +11,13 @@
  * Result to Print Out:
  *      "The tires pass spec!" or "Get your tires checked out!"
  */
+        fdt = Number(prompt("Enter Front Driver side tire pressure", 30));
+        fdt = Number(prompt("Enter Front Passenger side tire pressure", 30));
+        fdt = Number(prompt("Enter Rear Driver side tire pressure", 30));
+        fdt = Number(prompt("Enter Rear Passenger side tire pressure", 30));
 
+            var tparry = new Array();
+            tparry[0] = 30;
+            tparry[1] = 30;
+            tparry[2] = 32;
+            tparry[4] = 32;
