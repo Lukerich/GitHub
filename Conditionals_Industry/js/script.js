dbd8595884171f24be755832cc3abb885ea9ebb1
lukerich <r.l.luke@fullsail.edu>
2013-04-27 00:03:15 -0400

    Conditionals_Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,7 +2,7 @@
  * Richard Luke
  * Date: 4/22/13
  * Conditional_Industry
- * Choose the shipping method and calculate cost - validate complete address and shipping method selection
+ * Choose the shipping method and calculate cost  - validate complete address and shipping method selection
  *  Given:
  *      Shipping methods
  *      Cost of shipping
@@ -10,6 +10,7 @@
  *  Inputs:
  *       Address
  *       Shipping method
+ *       number of items
  *       Confirmation
  *  Results to print to console
  *       Address
@@ -28,6 +29,14 @@
             shpCst[WkHol][0]= 25;
             shpCst[SuCh][0]= 15;
 
+        // Prompt for number of items to ship - if 1-2 shipping price is 1 * array value
+        // if 3-5 shipping price is 1.5 * array value - if 6-10 shipping price is 2.5 * array
+        // above 10 users is alerted to call customer service for cost.
+        var noItm = Number(Prompt("Enter number of Items: ", ""));
+                if (noItm  <= 0){
+                    console.log("Number of Items must be entered:");
+                }
+
 
         // Assigning address array user input values
         adrss[0] = prompt("Enter First Name: ", "");
@@ -41,8 +50,8 @@
 
 // Assign variable for shipping method - standard (3-5), 2nd business day, overnight, weekend & holidays
                     // used as validation - must choose one method
-        shpMth = prompt("Enter Shipping method: \n Std, 2day, 1day, WkHol", "");
-                if (shpMth === "Std"){
+        var shpMth = prompt("Enter Shipping method: \n Std, 2day, 1day, WkHol", "");
+                if (shpMth === "Std" && noItm <= 2){
                     (adrss[5] !== "HI" || adrss[5] !== "AK" ) ? console.log("Shipping Cost $ 10.00") : console.log("Shipping Cost $ 25.00");
                 }else{
                 if (shpMth === "2day"){
