b1aac6aefb86376bfe69725052ff2cc4b651b13b
lukerich <r.l.luke@fullsail.edu>
2013-04-27 02:09:52 -0400

    Conditionals_Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -17,8 +17,8 @@
  *       Cost
  *
  */
-var mulFac
-var surChr
+var mulFac;
+var surChr;
 var adrss = new Array();   // array to hold the users address - validation on f-name, l-name, address, city, stat, zip code but not
 // second address/apt
 var shpCst = new Array();  // array to hold cost of ship method - regardless of location in Continental US, except Alaska -
