c41230adf7d91f61fc30f4dac7d1ff060735ce3d
lukerich <r.l.luke@fullsail.edu>
2013-04-28 00:48:57 -0400

    Conditionals_Personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,9 +8,9 @@
  *              20oz reusable coffee cup - $ 15.00 amortized over 1 year of coffee    .000913 cents at 3 cups a day each day for a year
  *              cost goes to  0.02704566210046 cent per 1oz
  *        12 cup coffee maker with 60oz carafe                    3.75 16oz cups - 3 20oz cups
- *        5 - 1/8 cup - 30ml coffee scoop per 60oz carafe   2 Tbls in an 1/8 = 10 Tbls per carafe = 59 grams per carafe = 2.08 ounces
+ *        5 - 1/8 cup - 30ml coffee scoop per 60oz carafe   2 Tbs in an 1/8 = 10 Tbs per carafe = 59 grams per carafe = 2.08 ounces
  *        a 12oz bag make 6 carafe      $ 1.25 per carafe     1oz ~ .021 cents       16oz ~ .34          20oz  ~ .42 cent
- *        25 lb/ft^3 * 1 tablespoon in grams in a 5.9 grams in a 1 Tbls fine ground coffee  - (http://www.ico.org/ecology.asp)
+ *        25 lb/ft^3 * 1 tablespoon in grams in a 5.9 grams in a 1 Tbs fine ground coffee  - (http://www.ico.org/ecology.asp)
  *        http://wiki.answers.com/Q/How_many_grams_of_coffee_are_in_a_tablespoon
  *        Coffee maker $40.00   amortization of coffee maker for one year of coffee brewing at 1 60oz carafe a day
  *        is  approx. 11 cents per carafe      1oz ~ .002 cents     16oz ~ .029 cents         20oz ~ .037 cents
@@ -21,7 +21,7 @@
  *        bag of 12oz ground coffee - $  7.50
  *        16 oz cup with lid ~ .25 cents
  *        20 oz cup with lid ~ .375 cents
- *        32 oz of half and half $ 1.70  2 Tbls per cup - 1/2fl oz per Tbls - 1oz. per cup  3.4 cent per cup  1.7 cents per Tbls
+ *        32 oz of half and half $ 1.70  2 Tbs per cup - 1/2fl oz per Tbs - 1oz. per cup  3.4 cent per cup  1.7 cents per Tbs
  *        10lb sugar $ 5.60 - 2 tsp per cup - 1 tsp = 4.2g -  .29 ounces of sugar per cup - 1 cent per cup .5 cent per tsp
  *   User Inputs
  *        Size of coffee in ounces
@@ -38,7 +38,8 @@ var vend = prompt("Enter M for McDonalds\xA9 or S for Starbucks\xA9:", "");
 var cosBC = Number(prompt("Enter cost for a bag of coffee: \n for home brewing.", ""));
 var bagSz = Number(prompt("Enter size of coffee bag in ounces: \n for home brewing.", ""));
 var venCCst = Number(prompt("Enter the cost of coffee from \n McDonalds\xA9 or Starbucks\xA9"));
-
+var amtSur = Number(prompt("Enter number of tsp of sugar:", ""));
+var amtHnH = Number(prompt("Enter number of Tbs"))
 // cost of coffee per oz. when brewed at home
 var cmCst = .002;
 var homCCst = ((cosBC / (bagSz / 2.08)) / 60);
@@ -49,6 +50,8 @@ var sugCst = .005;
 var hnhCst = 1.7;
 var cup16 = .25;
 var cup20 = .375;
+var cupRes = .000913 / cpsize;
+var basCC = (cmCst + homCCst + elcCst + waCst);
 
 if (cupTpe === "R"){
     cofCst = (cpsize * )
