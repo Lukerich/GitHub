acf06084fe240e40bdd84fbddc1c24f768cb45e0
lukerich <r.l.luke@fullsail.edu>
2013-04-28 12:10:36 -0400

    Conditionals_Personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -83,9 +83,9 @@ if (getCwh === "M" || getCwh === "m") {   // evalute user input for vendor being
 
 // cost of coffee bought at McDonalds or Starbucks
 var defvend = "McDoanlds\xA9";  // assign default vendor
-var defMDC16 = 1.49 / 16;            // assign default cost of 16oz coffee
-var defMDC20 = 1.69 / 20;            // assign default cost of 20oz coffee
-var defMDC32 = 1.89 / 32;            // assign default cost of 32oz coffee
+var defMDC16 = 1.49;            // assign default cost of 16oz coffee
+var defMDC20 = 1.69;            // assign default cost of 20oz coffee
+var defMDC32 = 1.89;            // assign default cost of 32oz coffee
 var bghtCC = (dyCC * venCCst);           // calculation of coffee at store
 
 // cost of coffee per oz. when brewed at home
@@ -109,12 +109,6 @@ if (getCwh === "H" || getCwh === "h") {      // if statement to evaluate where c
     if (cupTpe === "R") {
         bghtCC = (defMDC20 * dyCCh);            // assign default cost of 20oz coffee
         cofCst = ((cpHsize * basCC + (sugCst * amtSur) + (hnhCst * amtHnH) + cupRes) * dyCCh); // calculation for cost of coffee made at home with resuable cup
-        var surTst = sugCst * amtSur;
-        var hnhTst = hnhCst * amtHnH;
-        console.log(basCC);
-        console.log(cpHsize);
-        console.log(surTst);
-         console.log(hnhTst);
     } else {
         if (cpHsize >= 12 || cpHsize <= 16) {                                                   // evulation of cup size
             bghtCC = (defMDC16 * dyCCh);            // assign default cost of 16oz coffee
