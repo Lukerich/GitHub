62378ba4e50fad9acf22122356c2dcb2a3be3dc1
lukerich <r.l.luke@fullsail.edu>
2013-04-28 13:02:04 -0400

    Conditionals_Wacky

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,6 +2,12 @@
  * Richard Luke
  * Date: 4/26/13
  * Conditionals_Wacky
- *
+ * Monthly cost of pretzels
  */
+
+
+var przCst = Number(prompt("Enter cost of Pretzels per container:", ""));
+var Przamt = Number(prompt("Enter amount of Pretzels in container:", ""));
+var NumPrz = Number(prompt("Enter the amount of Pretzels consumed per day:", ""));
+
 
