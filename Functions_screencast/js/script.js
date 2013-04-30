b8e3f1cff1fc9dff014af42ba73a0cccb5060ea8
lukerich <r.l.luke@fullsail.edu>
2013-04-29 22:26:17 -0400

    functions_screencast

--- Functions_screencast/js/script.js
+++ Functions_screencast/js/script.js
@@ -11,3 +11,30 @@
 
 // Don't repeat your self (DRY)
 // functions store and reuse lines of code not values
+
+/**
+ *   Types
+ *   Functions a.k.a normal Functions
+ *   Procedures
+ *   Anonymous Functions
+ */
+/**
+ * Normal functions
+ * function functionName() {
+ * //code the function runs
+ * }
+ */
+      // Function execution
+// console.log("Hello World");  // code outside or out of scope of the function
+
+function outputMsg(){        //function and function name
+    console.log("Hello World");  // code or commands function runs
+}
+
+
+function calcArea(){       //function and function name
+    var width = 20;         //function and function name
+    var height = 30;
+    var area = width * height;
+    console.log("area");
+}
