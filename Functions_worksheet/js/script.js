0abe591926344c573412abe671d3fb23e065423b
lukerich <r.l.luke@fullsail.edu>
2013-04-30 23:10:19 -0400

    Conditionals_worksheet

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -11,26 +11,23 @@
  * Result to print to the console:
  * “The circumference of the circle is X”;
  */
-
+var total;
 var funSel = prompt("To calculate Circumference of a circle \n Enter R for radius or D for diameter.", "");
-    if (funSel === "R" || funSel === "r"){
-         cirCalc2(Number(prompt("Enter Radius of circle to calculate it's circumference", "")));
-        function cirCalc2(r){
-            var cirCalc2 = 2 * 3.14 * r;
-            return cirCalc2;
-        }
-        cirCalc2()
-        console.log("The circumference of the circle is " +  cirCalc2);
-    } else {
-        if (funSel === "D" || funSel === "d"){
-            cirCalc1(Number(prompt("Enter Radius of circle to calculate it's circumference", "")));
-            function cirCalc1(d){
-                var cirCalc1 = 3.14 * d;
-                return cirCalc1;
-            }
-            cirCalc1()
-            console.log("The circumference of the circle is " +  cirCalc1);
-        }
+if (funSel === "R" || funSel === "r") {
+    total = cirCalc2(Number(prompt("Enter Radius of circle to calculate it's circumference", "")));
+    function cirCalc2(r) {
+        var cirC = 2 * 3.14 * r;
+        return cirC;
     }
 
+} else {
+    if (funSel === "D" || funSel === "d") {
+        total = cirCalc1(Number(prompt("Enter Radius of circle to calculate it's circumference", "")));
+        function cirCalc1(d) {
+            var cirC = 3.14 * d;
+            return cirC;
+        }
+    }
+}
+console.log("The circumference of the circle is " + total);
 
