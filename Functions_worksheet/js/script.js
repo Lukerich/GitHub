0b222f4572e88a5d3e75803f8edc5012b3f2b688
lukerich <r.l.luke@fullsail.edu>
2013-04-30 22:04:34 -0400

    Conditionals_worksheet

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,6 +2,13 @@
  * Richard Luke
  * Date: 4/30/13
  * Conditionals_worksheet
- *
+ * Circumference
+ * Calculate the circumference of a circle.
+ * Parameter(s) for function:
+ * Radius of the circle
+ * Return:
+ * Circumference of the circle
+ * Result to print to the console:
+ * “The circumference of the circle is X”;
  */
 
