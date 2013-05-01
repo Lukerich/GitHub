46e5b0a3eea1e0987ebd39758614c762b32265e6
lukerich <r.l.luke@fullsail.edu>
2013-05-01 19:42:54 -0400

    Conditionals_worksheet

--- Functions_worksheet/js/stung.js
+++ Functions_worksheet/js/stung.js
@@ -15,17 +15,17 @@
  *      Result to print to the console:
  *      “It takes X bee stings to kill a X “
  */
-    // var stings;       // declare reportable variable
+    var morte;       // declare reportable variable
 
-    var stung = Number(prompt("Enter the victim's weight in pounds:", "")); // assign user input to variable to use as a parameter to feed the argument of the function
+     stung = Number(prompt("Enter the victim's weight in pounds:", "")); // assign user input to variable to use as a parameter to feed the argument of the function
     if (stung > 0){                                          // if statement to valid input is a number greater 0
-        function pain(lb){                                   // function defined or named as pain with parameter assigned
+        function stung(lb){                                   // function defined or named as pain with parameter assigned
             var stings = 8.6666666667 * lb;                   // formula assign to variable to calculate the number of stings to kill something or someone
             return stings;                                    // return to bring results out of the scope of the function
         }
     } else {                                                  // else of the if else statement to print out to the console a message if user input is not a number greater then 0
         console.log("You have to enter a number greater then 0"); // message printed to console for invalid input
     }
-
+   console.log("It takes " + )
 
 
