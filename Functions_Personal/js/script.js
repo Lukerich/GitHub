d07fa37028298fb2a6173907dae518289618340c
lukerich <r.l.luke@fullsail.edu>
2013-05-02 17:00:02 -0400

    Functions_Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,6 +6,7 @@
  * 4 miles / hour for 60 minutes for a 140lb person is ~ 330 calories
  *                        150 lb person 340 calories/hr.
  *                        175 lb person 396 calories/hr.
+ *                        median multiplier 2.2666666667 / 4 =  0.566666666675 / per mile
  * 10 lbs = 35000 calories burned
  * 1600 calories vs 2000 per day = 400
  * 1 hour walk at 4m/hr. = ~ 330   (140 lb person)
@@ -21,12 +22,19 @@ var input = [];      // array to hold input values
 input[0] = Number(prompt("Enter amount of weight in pounds you want to lose:", ""));
 input[1] = Number(prompt("Enter of calories you will reduce below base line of 2000:", ""));
 input[2] = Number(prompt("Enter number of miles you will walk at a 4miles/hr. pace per week:", ""));
-input[3] = Number(prompt("Enter your weight in pounds:", ""));
-    input [0, 1, 2, 3] = wgtLst(0, 1, 2, 3);
-    function wgtLst(a, b, c, d){
-            var timTls = a / (b + c * d);
+//input[3] = Number(prompt("Enter your weight in pounds:", ""));
 
-    }
+input [0, 1, 2, 3] = wgtLst(0, 1, 2);
+
+function wgtLst(a, b, c) {
+    var calPP = 3500;                       // calories burned per pound
+    var mulFct = 0.566666666675;               // multiplier for weight of person
+    var timTls = (a * calPP) / (b + c * mulFct);
+    return wgtLst;
+}
+total = wgtLst;
+
+console.log("It will take " + total + " days to lose " + input[0] + " lbs");
 
 
 
