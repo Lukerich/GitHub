b21a45757cd374571af1b406ac910a3c911eedce
lukerich <r.l.luke@fullsail.edu>
2013-05-02 16:12:43 -0400

    Functions_Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,6 +1,22 @@
 /**
  * Richard Luke
  * Date: 5/01/13
- * Functions_Personal
+ * Functions_Personal - time to lose weight
+ * 3500 calories per pound
+ * 4 miles / hour for 60 minutes for a 140lb person is ~ 330 calories
+ *                        150 lb person 340 calories/hr.
+ *                        175 lb person 396 calories/hr.
  */
+
+
+var total;      // declared variable to use as output to the console
+
+var input;      // array to hold input values
+
+input[0] = Number(prompt("Enter amount of weight in pounds you want to lose:", ""));
+input[1] = Number(prompt("Enter of calories you will reduce below base line of 2000:", ""));
+input[2] = Number(prompt("Enter number of miles you will walk at a 4miles/hr. pace per week:", ""));
+
+
+
 
