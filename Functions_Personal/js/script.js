ee69496289e759fc045a15e25ccb1c7e98ab0c83
lukerich <r.l.luke@fullsail.edu>
2013-05-02 17:37:24 -0400

    Functions_Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -20,13 +20,12 @@
 
 var input = [];      // array to hold input values
 
-input[0] = Number(prompt("Enter amount of weight in pounds you want to lose:", ""));
-input[1] = Number(prompt("Enter of calories you will reduce below base line of 2000:", ""));
-input[2] = Number(prompt("Enter number of miles you will walk at a 4miles/hr. pace per week:", ""));
+input[0] = wgtLst(a)(Number(prompt("Enter amount of weight in pounds you want to lose:", "")));
+input[1] = wgtLst(b)(Number(prompt("Enter of calories you will reduce below base line of 2000:", "")));
+input[2] = wgtLst(c)(Number(prompt("Enter number of miles you will walk at a 4miles/hr. pace per week:", "")));
 //input[3] = Number(prompt("Enter your weight in pounds:", ""));
 
 wgtTls = input[0];
-input [012]  = wgtLst(0, 1, 2);
 
 function wgtLst(a, b, c) {
     var calPP = 3500;                       // calories burned per pound
