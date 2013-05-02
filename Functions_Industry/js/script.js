0d36481108b9eef52f8cf9249a5add78d135606c
lukerich <r.l.luke@fullsail.edu>
2013-05-02 19:21:46 -0400

    Functions_Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,15 +2,25 @@
  * Richard Luke
  * Date: 5/02/13
  * Functions_Industry
+ * If production is line A then
  * bonus on production quota's
  * if production is 1000 then bonus is .01 each
  * if production is 1500 then bonus is .02 each
  * if production is 2000 then bonus is .03 each
  * if production is less then 1000 then no bonus
+ * If production is line B then
+ * bonus on production quota's
+ * if production is 1000 then bonus is .02 each
+ * if production is 1500 then bonus is .03 each
+ * if production is 2000 then bonus is .05 each
+ * if production is less then 500 then no bonus
  */
 
     var bonus = [];
 
-    var prdVal = (Number(prompt("Please enter shift production count:", "")));
+    var prdVal = (Number(prompt("Enter shift production count:", "")));
 
+    function lowPrd()
+
+    function prdHgh()
 
