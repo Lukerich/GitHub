68fad3d44e04deda41a1b85f6d5a9bac231dbafd
lukerich <r.l.luke@fullsail.edu>
2013-05-02 18:18:01 -0400

    Functions_Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -20,26 +20,23 @@ var total;      // declared variable to use as output to the console
 
 var input = [];      // array to hold input values
 
-input[0] = (Number(prompt("Enter amount of weight in pounds you want to lose:", "")));
-input[1] = (Number(prompt("Enter of calories you will reduce below base line of 2000:", "")));
-input[2] = (Number(prompt("Enter number of miles you will walk at a 4miles/hr. pace per day:", "")));
-input[3] = Number(prompt("Enter of days/week you will diet and exercise:", ""));
+input[0] = (Number(prompt("Enter amount of weight in pounds you want to lose:", "")));  // user input to array index 0 for lbs to lose
+input[1] = (Number(prompt("Enter of calories you will reduce below base line of 2000:", ""))); // user input to array index 1 for reduction in calories
+input[2] = (Number(prompt("Enter number of miles you will walk at a 4miles/hr. pace per day:", ""))); //user input to array index 2 for miles per day to walk
+input[3] = Number(prompt("Enter of days/week you will diet and exercise:", "")); //user input for number of days to exercise
 
-wgtTls = input[0];
+wgtTls = input[0];                          // variable to capture lbs to lose for output to console
 
-total = wgtLst(input[0], input[1], input[2], input[3]);
+total = wgtLst(input[0], input[1], input[2], input[3]);    // variable assigned to function return for out to console
 
-function wgtLst(a, b, c, d) {
+function wgtLst(a, b, c, d) {              // function definition with 4 inputs from array input
     var calPP = 3500;                       // calories burned per pound
     var mulFct = 85;                      // multiplier for weight of person
-    var timTls = (a * calPP) / (d * (b + c * mulFct));
-    var tst = (d * (b + c * mulFct));
-    console.log(tst);
-    return timTls;
-
+    var timTls = (a * calPP) / (d * (b + c * mulFct));  // time to lose variable assigned to result of formula
+    return timTls;                                   // return operation to bring results of function out of scope - to allow if to be used
 }
 
-console.log("It will take " + total + " weeks to lose " + wgtTls + " lbs");
+console.log("It will take " + total + " weeks to lose " + wgtTls + " lbs"); // output to console for testing
 
 
 
