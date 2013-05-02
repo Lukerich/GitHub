26da5f5001b0d009763641f0832e30805529f08b
lukerich <r.l.luke@fullsail.edu>
2013-05-01 20:26:51 -0400

    Conditionals_worksheet

--- Functions_worksheet/js/stung.js
+++ Functions_worksheet/js/stung.js
@@ -4,7 +4,7 @@
  * Functions_worksheet
  *
  * Stung!
- *
+ *                                // assign wgtLBs variable the number of pounds to use in reporting to console
  *      It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
  *      Given:
  *      Victim’s weight (in pounds)
@@ -19,11 +19,11 @@ var muerte;       // declared reportable variable
 var wgtLBs;       // declared variable to report weight of victim
 muerte = stung(Number(prompt("Enter the victim's weight in pounds:", ""))); // assign user input to variable to use as a parameter to feed the argument of the function
 if (muerte > 0) {                         // if statement to valid input is a number greater 0
-                                              // assign wgtLBs variable the number of pounds to use in reporting to console
-    wgtLBs = function stung(lb) {                                   // function defined or named as pain with parameter assigned
+    function stung(lb) {                                   // function defined or named as pain with parameter assigned
         var stings = 8.6666666667 * lb;                   // formula assign to variable to calculate the number of stings to kill something or someone
         return stings;                                    // return to bring results out of the scope of the function
     }
+
     console.log("It takes " + muerte + " bee stings to kill a " + wgtLBs + " creature");   // message to console reporting the number of bee stings to kill a creature of X lbs.
 } else {                                                  // else of the if else statement to print out to the console a message if user input is not a number greater then 0
     console.log("You have to enter a number greater then 0"); // message printed to console for invalid input
