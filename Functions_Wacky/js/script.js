864024499b5f43047e1304c476a885d0dd5b0745
lukerich <r.l.luke@fullsail.edu>
2013-05-02 22:21:44 -0400

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,6 +5,16 @@
  * ants go marching two by two
  */
 
-var ants;
+
+var annFun = function () {
 var ipAnt = [];
 ipAnt[0] = (Number(prompt("Enter the number of Ants marching", "")));
+ipAnt[1] = (Number(prompt("Enter how many minutes they march in", "")));
+ipAnt[2] = prompt("Are there more than two a second?", "");
+    if (ipAnt[2] == "Y" || "y"){
+       var anCt = (ipAnt[0] * ipAnt[1]);
+        console.log(anCt + "Ant's are marching in");
+    } else {
+        console.log("Ant's are marching ")
+    }
+}();
