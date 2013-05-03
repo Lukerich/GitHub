7ee3d0350a8e867ff196b4f21d00c26c2628c65b
lukerich <r.l.luke@fullsail.edu>
2013-05-02 22:59:09 -0400

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -13,7 +13,7 @@ var annFun = function () {               // an anonymous function to self run
     ipAnt[1] = (Number(prompt("Enter how many minutes they march in", "")));  // prompt for user input for array index 1
     ipAnt[2] = prompt("Are there more than two a second?", "");      // prompt for user input for array index 2
     if (ipAnt[2] !== "" && ipAnt[0] > 0 && ipAnt[1] > 0) {   // if statement to validate user input for prompts of array
-        if (ipAnt[2] == "Y" || "y") {                        // if statement to evaluate user input for Y or N
+        if (ipAnt[2] === "Y" || "y") {                        // if statement to evaluate user input for Y or N
             var anCt = (ipAnt[0] * ipAnt[1] * secIm);                    // variable assigned formula results
             console.log(anCt + " Ant's are marching in:");              // output to console for calculation results
         } else {                                                        // else of the if
