5038a9fb755d190c479788ad0f90af223dd52e4e
lukerich <r.l.luke@fullsail.edu>
2013-05-02 21:53:15 -0400

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,6 +1,10 @@
 /**
  * Richard Luke
- * Date: 5/01/13
+ * Date: 5/02/13
  * Functions_wacky
+ * ants go marching two by two
  */
 
+var ants;
+var ipAnt = [];
+var ipAnt[0] = (Number(prompt("Enter the number of Ants marching", "")));
