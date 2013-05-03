2ad4403fdc7dc11f32290a07bcdca6545c0e916b
lukerich <r.l.luke@fullsail.edu>
2013-05-02 23:20:19 -0400

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -14,11 +14,11 @@ var annFun = function () {               // an anonymous function to self run
     ipAnt[2] = prompt("Are there more than two a second?", "");      // prompt for user input for array index 2
 
 
-    if (ipAnt[2] === "Y" || "y" && (ipAnt[0] > 0 && ipAnt[1] > 0)) {   // if statement to validate user input for prompts of array
+    if (ipAnt[2] === ("Y" || "y") && (ipAnt[0] > 0 && ipAnt[1] > 0)) {   // if statement to validate user input for prompts of array
         var anCt = (ipAnt[0] * ipAnt[1] * secIm);                    // variable assigned formula results
         console.log(anCt + " Ant's are marching in:" + ipAnt[2] + ipAnt[0] + ipAnt[1]);              // output to console for calculation results
     } else {                                                        // else of the if
-        if (ipAnt[2] === "N" || "n") {                                  // if statement for N input
+        if (ipAnt[2] === ("N" || "n")) {                                  // if statement for N input
             console.log("Ant's are marching two by two:");               // output to the console when the input is N
         } else {
             console.log("Enter Y or N and a number greater then 0 for Ants and minutes:");  // output when validation failed
