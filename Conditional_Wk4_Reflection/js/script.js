b336bc369254fa044112960b39c8e78730839762
lukerich <r.l.luke@fullsail.edu>
2013-05-04 16:21:11 -0400

    Conditional_Wk4_Reflection

--- Conditional_Wk4_Reflection/js/script.js
+++ Conditional_Wk4_Reflection/js/script.js
@@ -9,13 +9,13 @@ ban4Sl[1] = prompt("Are there bananas for sale?:", ""); // array index 1 used fo
 if (ban4Sl[0] == "" || ban4Sl[1] == "") { // 1st if statement to validate user input is not blank
     console.log("Enter either Y or N:");    // output to user that blank is not allowed
 } else {                                    // else of if to evaluate user input
-    if (ban4Sl[0] === "Y" && ban4Sl[1] === "N") {  // if to evaluate user input for combination of Y and N
+    if (ban4Sl[0] == "Y" && ban4Sl[1] == "N") {  // if to evaluate user input for combination of Y and N
         console.log("Sorry there are no bananas today");  //output to user to show evaluation is true for Y and N input
     } else {                                            // else of if to evaluate user input for next combination
-        if (ban4Sl[0] === "Y" && ban4Sl[1] === "Y") {  //if to evaluate combination of Y and Y input
+        if (ban4Sl[0] == "Y" && ban4Sl[1] == "Y") {  //if to evaluate combination of Y and Y input
             console.log("The bananas are on aisle 5:");  // output to console when Y and Y input is true
         } else {                                          // else of if for next evaluation
-            if (ban4Sl[0] === "N") {                       // if to evaluate when 1st input is N
+            if (ban4Sl[0] == "N") {                       // if to evaluate when 1st input is N
                 console.log("Hmmm then why did you come to the Banana Store?:");  // output to user when 1st input is N
             } else {                                                              // else of if for last evaluation of user input
                 console.log("You enter a letter other then Y or N, this is not allowed:");   // output to user when all evaluations are false
