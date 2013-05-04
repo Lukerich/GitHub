b772b52c8556fb28c6b947e7c47858bb6c9fa2e2
lukerich <r.l.luke@fullsail.edu>
2013-05-04 16:07:15 -0400

    Conditional_Wk4_Reflection

--- Conditional_Wk4_Reflection/js/script.js
+++ Conditional_Wk4_Reflection/js/script.js
@@ -3,4 +3,20 @@
  * Date: 5/04/13
  * Conditional_Wk4_Reflection
  */
+var ban4Sl = [];
+    ban4Sl[0] = prompt("Do you want bananas?:", "Y,N");
+    ban4Sl[1] = prompt("Are there bananas for sale?:", "Y,N");
+    if (ban4Sl[0] === "" || ban4Sl[1] === ""){
+        console.log("Enter either Y or N:");
+    } else {
+        if (ban4Sl[0] === "Y" && ban4Sl[1] === "N");
+        console.log("Sorry there are no bananas today");
+    } else {
+    if (ban4Sl[0] === "Y" && ban4Sl[1] === "Y");
+        console.log("The bananas are on aisle 5:");
+} else {
+    if ban4Sl[0 === "N"];
+    console.log("Hmmm then why did you come to the Banana Store?:");
+}
+
 
