b69d4cac45f7cf4d0246f3a07d2771bcc0548038
lukerich <r.l.luke@fullsail.edu>
2013-05-04 16:13:51 -0400

    Conditional_Wk4_Reflection

--- Conditional_Wk4_Reflection/js/script.js
+++ Conditional_Wk4_Reflection/js/script.js
@@ -3,20 +3,26 @@
  * Date: 5/04/13
  * Conditional_Wk4_Reflection
  */
-var ban4Sl = [];
-    ban4Sl[0] = prompt("Do you want bananas?:", "Y,N");
-    ban4Sl[1] = prompt("Are there bananas for sale?:", "Y,N");
-    if (ban4Sl[0] === "" || ban4Sl[1] === ""){
-        console.log("Enter either Y or N:");
-    } else {
-        if (ban4Sl[0] === "Y" && ban4Sl[1] === "N");
+var ban4Sl = [];                      // variable assigned to array for user input
+ban4Sl[0] = prompt("Do you want bananas?:", "Y,N"); //array index 0 used for 1st user input
+ban4Sl[1] = prompt("Are there bananas for sale?:", "Y,N"); // array index 1 used for 2nd user input
+if (ban4Sl[0] === "" || ban4Sl[1] === "") { // 1st if statement to validate user input is not blank
+    console.log("Enter either Y or N:");
+} else {
+    if (ban4Sl[0] === "Y" && ban4Sl[1] === "N") {
         console.log("Sorry there are no bananas today");
     } else {
-    if (ban4Sl[0] === "Y" && ban4Sl[1] === "Y");
-        console.log("The bananas are on aisle 5:");
-} else {
-    if ban4Sl[0 === "N"];
-    console.log("Hmmm then why did you come to the Banana Store?:");
+        if (ban4Sl[0] === "Y" && ban4Sl[1] === "Y") {
+            console.log("The bananas are on aisle 5:");
+        } else {
+            if (ban4Sl[0] === "N") {
+                console.log("Hmmm then why did you come to the Banana Store?:");
+            } else {
+                console.log("You enter a letter other then Y or N, this is not allowed:");
+            }
+        }
+    }
 }
+
 
 
